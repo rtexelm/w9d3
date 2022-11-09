2fe46940c8589b5680e6bc490c3958e080f9ed85
rtexelm <texelist@gmail.com>
2022-11-09 16:02:30 -0500

    Moving to toggle button listener

--- ajax_twitter/app/javascript/follow_toggle.js
+++ ajax_twitter/app/javascript/follow_toggle.js
@@ -3,12 +3,15 @@ import { API, broadcast } from "./util";
 export default class FollowToggle {
   constructor(toggleButton) {
     // Your code here
-    // this.toggleButton = toggleButton.dataset.userId = 1;
+    this.toggleButton = toggleButton;
+    this.handleClick = this.handleClick.bind(this)
+    // console.log(this)
   }
 
 
   async handleClick(event) {
-    // Your code here
+    event.preventDefault()
+    console.log(this.followState)
   }
 
   async follow() {
